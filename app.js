b285e396b3d8b9b8755376e0c7c41cf6b390ad72
Ben Dunn <benthedunn@gmail.com>
2020-07-28 15:50:12 -0600

    MVP functional

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ card3.addEventListener( 'click', function() {
 const $bundle1 = document.querySelector(".thecard1")
 const $bundle2 = document.querySelector(".thecard2")
 const $bundle3 = document.querySelector(".thecard3")
+// const $facecard = document.querySelector(".facecard")
 const $bundleTag = document.createElement("div")
 const $maincontainer = document.querySelector(".maincontainer")
 const baseURL = "http://localhost:3000/cards/" 
@@ -24,28 +25,40 @@ fetch(baseURL)
     .then(displayCards)
 
 function displayCards(cards){
+
+    
     const $card1 = random(1,26).toFixed()
     const $card2 = random(27,52).toFixed()
     const $card3 = random(53,78).toFixed()
 
-    console.log($card1)
-    $bundle1.append(eachCard(cards, "1", $card1)) 
-    $bundle2.append(eachCard(cards, "2", $card2)) 
-    $bundle3.append(eachCard(cards, "3", $card3)) 
+  
+    
+    $bundle1.append(eachCard(cards, "1", $card1)[0], eachCard(cards, "1", $card1)[1]) 
+    $bundle2.append(eachCard(cards, "2", $card2)[0], eachCard(cards, "2", $card2)[1]) 
+    $bundle3.append(eachCard(cards, "3", $card3)[0], eachCard(cards, "3", $card3)[1]) 
+    console.log(eachCard(cards, "1", $card1)[1])
     $maincontainer.append($bundle1, $bundle2, $bundle3)
 }
 
 function eachCard(card, $bundle, $cardID){
+    const $thefront = document.createElement("div")
+    $thefront.classList.add("thefront")
+
+    const $firstImage = document.createElement('img')
+    $firstImage.src = "https://www.wopc.co.uk/images/subjects/tarot/rider-waite/pam-roses-lilies-back.jpg"
+    $thefront.append($firstImage)
+    
     const $chosenCard = card.filter(card => card.bundle === $bundle && card.id == $cardID)
-    bundleCard($chosenCard[0].name, $chosenCard[0].desc, $chosenCard[0].image)
+    const oneCard = bundleCard($chosenCard[0].name, $chosenCard[0].desc, $chosenCard[0].image)
+    // $bundleTag.append(oneCard)
+    return [$thefront, oneCard];  
     // console.log($chosenCard[0].name, $chosenCard[0].desc, $chosenCard[0].image)
 }
 
 
-function bundleCard(name, desc, image){
+function bundleCard(name, desc, cardImage){
     // console.log(bundle.id, "got the name")
-    const $thefront = document.createElement("div")
-    $thefront.classList.add("thefront")
+    
     const $theback = document.createElement("div")
     $theback.classList.add("theback")
     const $facecard = document.createElement("div")
@@ -54,14 +67,14 @@ function bundleCard(name, desc, image){
     $description.classList.add("description")
     
     //the first image
-    const $firstImage = document.createElement('img')
-    $firstImage.src = "https://www.wopc.co.uk/images/subjects/tarot/rider-waite/pam-roses-lilies-back.jpg"
-    $thefront.append($firstImage)
+    // const $firstImage = document.createElement('img')
+    // $firstImage.src = "https://www.wopc.co.uk/images/subjects/tarot/rider-waite/pam-roses-lilies-back.jpg"
+    // $thefront.append($firstImage)
 
 
     //the face card after flipped
     const $image = document.createElement('img')
-    $image.src = image
+    $image.src = `${cardImage}`
     $facecard.append($image)
 
     //description div
@@ -75,8 +88,8 @@ function bundleCard(name, desc, image){
     $theback.append($facecard, $description)
 
     //a single bundle
-    return $thefront, $thefront
-    // $bundleTag.append($thefront,$theback)
+    return $theback
+    // return $bundleTag.append($thefront,$theback)
     // // $bundle2.append($thefront,$theback)
     // // $bundle3.append($thefront,$theback)
     // $maincontainer.append($bundleTag)
